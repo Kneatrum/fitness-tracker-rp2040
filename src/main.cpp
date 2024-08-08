6db4c3de27f1bbf5e5b09e890b5e6f77ada2ad3a
Kneatrum <martin.mwiti.n@gmail.com>
2024-08-08 12:11:05 +0300

    Successfull mqtt and wifi connection. Pending heart rate monitor

--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,7 @@
 #include <WiFiUdp.h>
 #include <Wire.h>
 #include "MAX30100_PulseOximeter.h"
+// #include "imufx.h"
 
 const char* ssid = SSID;
 const char* password = WIFIPASSWORD;
@@ -29,7 +30,7 @@ NTPClient timeClient(ntpUDP, "pool.ntp.org", utcOffsetInSeconds, 60000);  // NTP
 
 int status = WL_IDLE_STATUS;
 
-const char broker[] = "192.168.100.10"; // IP address of the mqtt host. 
+const char broker[] = "192.168.101.47"; // IP address of the mqtt host. 
 
 int mqtt_port = 1883;
 
@@ -58,6 +59,32 @@ bool subscribed = false;
 int last_tiggered_second = -1;
 int heart_last_tiggered_second = -1;
 
+
+// uint16_t step_count = 0;
+char report[256];
+uint32_t previous_tick;
+
+
+void mqtt_send_int(const char topic[], int data){
+  mqttClient.beginMessage(topic);
+  mqttClient.print(data);
+  mqttClient.endMessage();
+}
+
+void mqtt_send_str(const char topic[], char* data){
+  mqttClient.beginMessage(topic);
+  mqttClient.print(data);
+  mqttClient.endMessage();
+}
+
+void mqtt_send_float(const char topic[], float data){
+  char buffer[10];
+  mqttClient.beginMessage(topic);
+  sprintf(buffer, "%.2f", data);  // 2 decimal places
+  mqttClient.print(buffer);
+  mqttClient.endMessage();
+}
+
 void pinInit(){
   pinMode(LED_BUILTIN, OUTPUT);
   pinMode(LEDR, OUTPUT);
@@ -70,29 +97,43 @@ void pinInit(){
 }
 
 bool wifiConnect(){
-  status = WiFi.begin(ssid, password);
-  if ( status != WL_CONNECTED) return false;
-  return true; 
+  uint8_t count = 0;
+  Serial1.print("Connecting to WIFI");
+  while(status != WL_CONNECTED){
+    status = WiFi.begin(ssid, password);
+    delay(1000);
+    count++;
+    Serial1.print(".");
+    
+    if(count == 10){
+      Serial1.println("\nFailed to connect to WIFI");
+      return false;
+    }
+  }
+  Serial1.println();
+  Serial1.println("Connected to WIFI");
+  return true;
 }
 
 
 
 
 bool mqttConnect() {
-  // Serial.print("checking wifi...");
-  // while (WiFi.status() != WL_CONNECTED) {
-  //   Serial.print(".");
-  //   delay(1000);
-  // }
-
-  Serial.print("\nconnecting...");
-  if (!mqttClient.connect(broker, mqtt_port)) {
-    Serial.print("MQTT connection failed! Error code = ");
-    Serial.println(mqttClient.connectError());
-    return false;
+  uint8_t count = 1;
+  Serial1.println("\nConnecting to MQTT broker");
+  while (!mqttClient.connect(broker, mqtt_port)) {
+    Serial1.print("Connecttion attempt ");
+    Serial1.print(count);
+    Serial1.print(" Error code = ");
+    Serial1.println(mqttClient.connectError());
+    count++;
+    if(count == 5){
+      Serial1.println("MQTT connection failed!");
+      return false;
+    }
   }
 
-  Serial.println("You're connected to the MQTT broker!");
+  Serial1.println("You're connected to the MQTT broker!");
   return true;
   
 }
@@ -106,34 +147,37 @@ void onBeatDetected()
 }
 
 void setup() {
-  Serial1.begin(115200);
-  pinInit();
+    // Wire.begin();
+    Serial1.begin(115200);
+    pinInit();
 
-  // while(!Serial);
-  if(!IMU.begin()){
-    Serial1.println("Failed to initialize IMU!");
-    while (1);
-  }
+    // while(!Serial);
+    // if(!IMU.begin()){
+    //     Serial1.println("Failed to initialize IMU!");
+    //     while (1);
+    // }
 
-  // Connect to wifi
-  if(wifiConnect()){
-    wifi_connected = true;
-  } 
+    // Connect to wifi
+    if(wifiConnect()){
+      wifi_connected = true;
+      if(mqttConnect()){
+          mqtt_connected = true;
+      } 
+    }
 
-  // Initialize a NTPClient to get time
-  timeClient.begin();
+    // Initialize a NTPClient to get time
+    timeClient.begin();
 
-  if(mqttConnect()){
-    mqtt_connected = true;
-  } 
 
-  while (!pox.begin()) {
-    Serial1.print("Initializing pulse oximeter..");
-    delay(1000);
-  }
+    while (!pox.begin()) {
+        Serial1.print("Initializing pulse oximeter..");
+        delay(1000);
+    }
 
   pox.setOnBeatDetectedCallback(onBeatDetected);
    
+  //  imu_mlc_init();
+  Serial1.println("\nHello world");
 }
 
 void loop() {
@@ -151,28 +195,62 @@ void loop() {
 
   // publish a message roughly every second.
   if( currentSecond % 5 == 0 && last_tiggered_second != currentSecond){
-    if(IMU.temperatureAvailable()){
-      float temperature_float = 0;
-      IMU.readTemperatureFloat(temperature_float);
+    // if(IMU.temperatureAvailable()){
+    //   float temperature_float = 0;
+    //   IMU.readTemperatureFloat(temperature_float);
 
-      mqttClient.beginMessage(temperature);
-      sprintf(buffer, "%.2f", temperature_float);  // 2 decimal places
-      mqttClient.print(buffer);
-      mqttClient.endMessage();
-    }
+    //   mqttClient.beginMessage(temperature);
+    //   sprintf(buffer, "%.2f", temperature_float);  // 2 decimal places
+    //   mqttClient.print(buffer);
+    //   mqttClient.endMessage();
+    // }
+    Serial1.println("Alive");
     last_tiggered_second =  currentSecond;
   }
 
   int heartCurrentSecond = timeClient.getSeconds();
-  if( heartCurrentSecond % 5 == 0 && heart_last_tiggered_second != heartCurrentSecond && time_to_print){
-    // Serial1.print(pox.getSpO2());
+  if( heartCurrentSecond % 2 == 0 && heart_last_tiggered_second != heartCurrentSecond && time_to_print){
+    Serial1.print(pox.getSpO2());
     mqttClient.beginMessage(heart);
     float result = pox.getHeartRate();
+    Serial1.print("Heart rate = ");
+    Serial1.println(result);
     sprintf(buffer, "%.2f", result);  // 2 decimal places
     mqttClient.print(buffer);
     mqttClient.endMessage();
     heart_last_tiggered_second = heartCurrentSecond;
     time_to_print = false;
   }
+
+// if (mems_event)
+//   {
+//     mems_event=0;
+//     LSM6DSOX_MLC_Status_t mlc_status;
+//     AccGyr.Get_MLC_Status(&mlc_status);
+//     LSM6DSOX_Event_Status_t status;
+//     AccGyr.Get_X_Event_Status(&status);
+
+//     if (mlc_status.is_mlc1) {
+//       uint8_t mlc_out[8];
+//       AccGyr.Get_MLC_Output(mlc_out);
+//       printMLCStatus(mlc_out[0]);
+//     }
+    
+//     if (status.StepStatus)
+//     {
+//       // New step detected, so print the step counter
+//       AccGyr.Get_Step_Count(&step_count);
+//     }
+//   }
+  
+//   // Print the step counter in any case every 3000 ms
+//   uint32_t current_tick = millis();
+//   if((current_tick - previous_tick) >= 3000)
+//   {
+//     snprintf(report, sizeof(report), "#Step counter: %d", step_count);
+//     Serial1.println(report);
+//     mqtt_send_str(steps, report);
+//     previous_tick = millis();
+//   }
 
 }
